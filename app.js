172eb4cc682dfe08a58149214787ced39283c668
tobywatts <tobywatts04@gmail.com>
2025-04-27 16:04:21 +0100

    Remove node modules

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve your index.html for form submission
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // // POST route to handle form submission
 // app.post('/submit', async (req, res) => {
 //   const { username, email, password, current_height, current_weight, sex, date_of_birth } = req.body;
-  
+
 //   try {
 //     // Insert form data into the database
 //     const [rows, fields] = await pool.execute(
-//       'INSERT INTO users (username, email, password, current_height, current_weight, sex, date_of_birth) VALUES (?, ?, ?, ?, ?, ?, ?)', 
+//       'INSERT INTO users (username, email, password, current_height, current_weight, sex, date_of_birth) VALUES (?, ?, ?, ?, ?, ?, ?)',
 //       [username, email, password, current_height, current_weight, sex, date_of_birth]
 //     );
 //     res.send('Data successfully inserted into the database!');
@@ -26,7 +26,5 @@ app.use(express.static('public'));
 // });
 
 const server = app.listen(3000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-  });
-
-  
+  console.log(`Express running → PORT ${server.address().port}`);
+});
